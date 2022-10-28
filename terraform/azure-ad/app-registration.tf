0fc9fd71816067821bebfdbd9f9463e717d5603e
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2022-10-28 08:39:18 -0230

    Change logo; format and add comments

--- terraform/azure-ad/app-registration.tf
+++ terraform/azure-ad/app-registration.tf
@@ -5,12 +5,11 @@
 # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/application
 # #############################################################################
 
-resource "random_uuid" "app_role_application_manage_id" {}
-resource "random_uuid" "app_role_passport_status_read_id" {}
-resource "random_uuid" "app_role_passport_status_read_all_id" {}
-resource "random_uuid" "app_role_passport_status_write_id" {}
-resource "random_uuid" "app_role_passport_status_write_all_id" {}
-resource "random_uuid" "spn_application_id" {}
+data "azuread_users" "application_owners" {
+  # Application registraion owners have the ability to view and edit an application registration.
+  # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/data-sources/users
+  user_principal_names = var.resource_owners
+}
 
 data "azuread_users" "service_principal_owners" {
   # Enterprise application owners have the ability to manage all aspects of an enterprise application.
@@ -19,25 +18,13 @@ data "azuread_users" "service_principal_owners" {
   user_principal_names = var.resource_owners
 }
 
-resource "azuread_service_principal" "passport_status" {
-  # AAD enterprise applications are just specialized service principals
-  # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/service_principal
-
-  application_id               = azuread_application.passport_status.application_id
-  app_role_assignment_required = true
-  owners                       = data.azuread_users.service_principal_owners.object_ids
-
-  feature_tags {
-    enterprise = true
-    hide       = true
-  }
-}
-
-data "azuread_users" "application_owners" {
-  # Application registraion owners have the ability to view and edit an application registration.
-  # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/data-sources/users
-  user_principal_names = var.resource_owners
-}
+# Some application registration resources (ie: app roles) require user-provided IDs
+resource "random_uuid" "app_role_application_manage_id" {}
+resource "random_uuid" "app_role_passport_status_read_id" {}
+resource "random_uuid" "app_role_passport_status_read_all_id" {}
+resource "random_uuid" "app_role_passport_status_write_id" {}
+resource "random_uuid" "app_role_passport_status_write_all_id" {}
+resource "random_uuid" "spn_application_id" {}
 
 resource "azuread_application" "passport_status" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/application
@@ -46,8 +33,8 @@ resource "azuread_application" "passport_status" {
   identifier_uris       = var.application_identifier_uris
   logo_image            = filebase64("assets/logo.png")
   owners                = data.azuread_users.application_owners.object_ids
-  privacy_statement_url = "https://www.canada.ca/en/transparency/privacy.html"
-  terms_of_service_url  = "https://www.canada.ca/en/transparency/terms.html"
+  privacy_statement_url = var.application_privacy_statement_url
+  terms_of_service_url  = var.application_terms_of_service_url
 
   api {
     requested_access_token_version = 2
@@ -98,19 +85,22 @@ resource "azuread_application" "passport_status" {
   }
 
   web {
-    homepage_url = "https://www.canada.ca/en.html"
+    homepage_url = var.application_web_homepage_url
   }
 
   lifecycle {
     ignore_changes = [
       # TODO :: GjB :: figure out how to get this working
       #
-      # Explanation: we typically configure the main Passport Status OAuth client
-      # in Swagger to allow developers to authenticate to access protected endpoints.
-      # For that to work, we have to add all of the app_roles above to the azuread_application,
-      # but since that requires knowing the application id (client id), we can't do it here.
-      # That mapping is done afterwards in the Azure portal, which ultimately alterst he
-      # required_resource_access attribute, triggering terraform.
+      # Explanation:
+      #   We typically configure the main Passport Status OAuth client in
+      #   Swagger to allow developers to authenticate to access protected
+      #   endpoints. For that to work, we have to add all of the app_roles
+      #   defined above to the azuread_application resource, but since that
+      #   requires knowing the application id (client id), we can't do it here
+      #   (chicken/egg problem). That mapping is done afterwards in the Azure
+      #   portal, which ultimately alterst he required_resource_access
+      #   attribute, triggering terraform.
       required_resource_access
     ]
   }
@@ -122,47 +112,68 @@ resource "azuread_application_password" "passport_status" {
   end_date_relative     = "876000h"
 }
 
+resource "azuread_service_principal" "passport_status" {
+  # AAD enterprise applications are just specialized service principals
+  # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/service_principal
+
+  application_id               = azuread_application.passport_status.application_id
+  app_role_assignment_required = true
+  owners                       = data.azuread_users.service_principal_owners.object_ids
+
+  feature_tags {
+    enterprise = true
+    hide       = true
+  }
+}
+
 # #############################################################################
 # Passport Status SPN app role assignments...
+# (essentially assign all roles to `self`)
 # #############################################################################
 
 resource "azuread_app_role_assignment" "app_role_application_manage_admin_consent" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
-  app_role_id         = random_uuid.app_role_application_manage_id.result
   principal_object_id = azuread_service_principal.passport_status.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_application_manage_id.result # Application.Manage
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 resource "azuread_app_role_assignment" "app_role_passport_status_read_admin_consent" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
-  app_role_id         = random_uuid.app_role_passport_status_read_id.result
   principal_object_id = azuread_service_principal.passport_status.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_read_id.result # PassportStatus.Read
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 resource "azuread_app_role_assignment" "app_role_passport_status_read_all_admin_consent" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
-  app_role_id         = random_uuid.app_role_passport_status_read_all_id.result
   principal_object_id = azuread_service_principal.passport_status.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_read_all_id.result # PassportStatus.Read.All
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 resource "azuread_app_role_assignment" "app_role_passport_status_write_admin_consent" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
-  app_role_id         = random_uuid.app_role_passport_status_write_id.result
   principal_object_id = azuread_service_principal.passport_status.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_write_id.result # PassportStatus.Write
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 resource "azuread_app_role_assignment" "app_role_passport_status_write_all_admin_consent" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
-  app_role_id         = random_uuid.app_role_passport_status_write_all_id.result
   principal_object_id = azuread_service_principal.passport_status.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_write_all_id.result # PassportStatus.Write.All
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 # #############################################################################
 # 3rd party SPN app role assignments...
+# (service accounts that need to authenticate in order to call our API)
 # #############################################################################
 
 data "azuread_service_principal" "interop" {
@@ -172,13 +183,15 @@ data "azuread_service_principal" "interop" {
 
 resource "azuread_app_role_assignment" "app_role_interop_write_all_admin_consent" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
-  app_role_id         = random_uuid.app_role_passport_status_write_all_id.result
   principal_object_id = data.azuread_service_principal.interop.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_write_all_id.result # PassportStatus.Write.All
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 # #############################################################################
 # User/group app role assignments...
+# (users who can authenticate, typically using authcode w/ pkce)
 # #############################################################################
 
 data "azuread_users" "app_role_application_manage_users" {
@@ -190,9 +203,10 @@ resource "azuread_app_role_assignment" "app_role_application_manage" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
   for_each = { for user in data.azuread_users.app_role_application_manage_users.users : user.user_principal_name => user }
 
-  app_role_id         = random_uuid.app_role_application_manage_id.result
   principal_object_id = each.value.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_application_manage_id.result # Application.Manage
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 data "azuread_users" "app_role_passport_status_read_users" {
@@ -204,9 +218,10 @@ resource "azuread_app_role_assignment" "app_role_passport_status_read" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
   for_each = { for user in data.azuread_users.app_role_passport_status_read_users.users : user.user_principal_name => user }
 
-  app_role_id         = random_uuid.app_role_passport_status_read_id.result
   principal_object_id = each.value.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_read_id.result # PassportStatus.Read
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 data "azuread_users" "app_role_admin_passport_status_read_users" {
@@ -218,9 +233,10 @@ resource "azuread_app_role_assignment" "app_role_admin_passport_status_read" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
   for_each = { for user in data.azuread_users.app_role_admin_passport_status_read_users.users : user.user_principal_name => user }
 
-  app_role_id         = random_uuid.app_role_passport_status_read_all_id.result
   principal_object_id = each.value.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_read_all_id.result # PassportStatus.Read.All
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 data "azuread_users" "app_role_passport_status_write_users" {
@@ -230,11 +246,13 @@ data "azuread_users" "app_role_passport_status_write_users" {
 
 resource "azuread_app_role_assignment" "app_role_passport_status_write" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
+
   for_each = { for user in data.azuread_users.app_role_passport_status_write_users.users : user.user_principal_name => user }
 
-  app_role_id         = random_uuid.app_role_passport_status_write_id.result
   principal_object_id = each.value.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_write_id.result # PassportStatus.Write
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
 
 data "azuread_users" "app_role_admin_passport_status_write_users" {
@@ -246,7 +264,8 @@ resource "azuread_app_role_assignment" "app_role_admin_passport_status_write" {
   # see: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment
   for_each = { for user in data.azuread_users.app_role_admin_passport_status_write_users.users : user.user_principal_name => user }
 
-  app_role_id         = random_uuid.app_role_passport_status_write_all_id.result
   principal_object_id = each.value.object_id
-  resource_object_id  = azuread_service_principal.passport_status.object_id
+
+  app_role_id        = random_uuid.app_role_passport_status_write_all_id.result # PassportStatus.Write.All
+  resource_object_id = azuread_service_principal.passport_status.object_id
 }
--- terraform/azure-ad/variables.tf
+++ terraform/azure-ad/variables.tf
@@ -4,25 +4,47 @@ variable "resource_owners" {
   default     = []
 }
 
-variable application_display_name {
+#
+# Application registration (OAuth client)
+#
+
+variable "application_display_name" {
   description = "The display name to use for the Azure application registration."
   type        = string
 }
 
-variable application_identifier_uris {
+variable "application_identifier_uris" {
   description = "A set of user-defined URIs that uniquely identify an application within its Azure AD tenant."
   type        = set(string)
 }
 
-variable application_spa_redirect_uris {
+variable "application_privacy_statement_url" {
+  description = "URL of the application's privacy statement."
+  type        = string
+  default     = "https://www.canada.ca/en/transparency/privacy.html"
+}
+
+variable "application_spa_redirect_uris" {
   description = "A set of URLs where user tokens are sent for sign-in, or the redirect URIs where OAuth 2.0 authorization codes and access tokens are sent."
   type        = set(string)
   default     = []
 }
 
-variable interop_service_principal_name {
+variable "interop_service_principal_name" {
   description = "The service principal name used by the interop team to interface with this application."
   type        = string
+}
+
+variable "application_terms_of_service_url" {
+  description = "URL of the application's terms of service statement."
+  type        = string
+  default     = "https://www.canada.ca/en/transparency/terms.html"
+}
+
+variable "application_web_homepage_url" {
+  description = "Home page or landing page of the application."
+  type        = string
+  default     = "https://www.canada.ca/en.html"
 }
 
 #
